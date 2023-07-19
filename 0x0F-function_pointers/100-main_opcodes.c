802729a0d1ff184b7dc7b9699a559f6e500c451c
Khakhathi94 <goddymulaudzi@gmail.com>
2023-07-19 10:51:11 -0700

    Updated file

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - prints its own opcodes
  * @argc: number of arguments
@@ -17,14 +18,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
 	{
-	     printf("Error\n");
+		printf("Error\n");
 		exit(2);
 	}
+
 	arr = (char *)main;
+
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
@@ -36,3 +40,4 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
